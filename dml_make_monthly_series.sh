50fe9c0c585f720ba2e852238f6c5a371c5223df
Mikhail Itkin <itkin.m@gmail.com>
2017-05-23 13:20:43 +0200

    add check for correct month value

--- dml_make_monthly_series.sh
+++ dml_make_monthly_series.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
+# USAGE: ./dml_make_monthly_mean.sh -o <output_directory>
+#                                   -d <input_directory>
+#                                   -m <month>
+#                                   -f force creating output directory
+
 set -e
+
+function make_monthly_mean() {
+
+  input_dir=$1
+  outdir=$2
+  month=$3
+  fileglob="seaice_conc_monthly_sh_???_????${month}_v02r00.nc"
+  echo $fileglob $input_dir $outdir $month
+
+  merged_monthly_ncfile=$outdir/merged_seaice_conc_monthly_${month}.nc
+  monthly_mean_ncfile=$outdir/monthly_seaice_conc_${month}.nc
+
+  # concatenate the entire series series to monthly files
+  cdo mergetime ${input_dir}/${fileglob} $merged_monthly_ncfile
+  cdo setdate,1978-${month}-15 -timmean ${merged_monthly_ncfile} ${monthly_mean_ncfile}.time.nc
+}
 
 # Handle the command line options
 while [ "$#" -gt 0 ]; do
@@ -23,21 +44,9 @@ if [ ! -d $OUTPUT_DIR ]; then
   fi
 fi
 
-function make_monthly_mean() {
-  input_dir=$1
-  outdir=$2
-  month=$3
-  fileglob="seaice_conc_monthly_sh_???_????${month}_v02r00.nc"
-  echo $fileglob $input_dir $outdir $month
-
-  merged_monthly_ncfile=$outdir/merged_seaice_conc_monthly_${month}.nc
-  monthly_mean_ncfile=$outdir/monthly_seaice_conc_${month}.nc
-
-  # concatenate the entire series series to monthly files
-  cdo mergetime ${input_dir}/${fileglob} $merged_monthly_ncfile
-  cdo setdate,1978-${month}-15 -timmean ${merged_monthly_ncfile} ${monthly_mean_ncfile}.time.nc
-}
+if [ "$MONTH" -gt 12 ] || [ "$MONTH" -lt 1 ]; then
+  echo "MONTH value $MONTH is not valid, must be from 1 to 12"; exit 1
+fi
 
 MONTH=$(printf %02d $MONTH)
 make_monthly_mean ${INPUT_DIR} ${OUTPUT_DIR} ${MONTH}
-
