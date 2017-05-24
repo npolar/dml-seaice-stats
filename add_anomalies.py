53c56147673a8b0b79e7449a5f768112907d5e55
Mikhail Itkin <itkin.m@gmail.com>
2017-05-24 17:03:29 +0200

    add anomalies to the netcdf files

--- add_anomalies.py
+++ add_anomalies.py
@@ -6,18 +6,29 @@ import netCDF4 as nc
 
 def add_anomaly_var(filepath, src_var='goddard_merged_seaice_conc_monthly'):
     dst = nc.Dataset(filepath, 'a')
-    try:
-        dst.createVariable('sic_anomaly',
+    if 'sic_anomaly' in dst.variables.keys():
+        pass
+    else:
+        try:
+            dst.createVariable('sic_anomaly',
                            numpy.float32,
                            dimensions=(dst.variables[src_var].dimensions))
-    except RuntimeError:
-        raise ValueError('Cannot user variable name {}'.format('sic_anomaly'))
+        except RuntimeError:
+            raise Warning('Cannot use variable name {}'.format('sic_anomaly'))
+    return dst
+
+
+def compute_anomalies(array):
+    mean = array.mean(axis=(0,))
+    anomalies = array - mean
+    return anomalies
 
 
 def main():
     infile = sys.argv[1]
-    add_anomaly_var(infile)
-    dst_anomalies = compute_anomalies(infile)
+    dst = add_anomaly_var(infile)
+    sic = dst.variables['goddard_merged_seaice_conc_monthly'][:]
+    dst_anomalies = compute_anomalies(sic)
     with nc.Dataset(infile, 'a') as dst:
         dst.variables['sic_anomaly'][:] = dst_anomalies
 
